3ae6bf5b5186434e8426b50d464e014024b6b8a3
David Cassel <dmcassel@gmail.com>
2023-03-27 15:47:03 -0400

    Test as writer

--- src/test/ml-modules/root/test/suites/recipes/recipeComments/addCommentToExisting.mjs
+++ src/test/ml-modules/root/test/suites/recipes/recipeComments/addCommentToExisting.mjs
@@ -3,12 +3,13 @@ declareUpdate();
 const { addCommentToRecipe, getCommentsFromRecipe } = require("/lib/recipes.sjs");
 const { runAsUpdate, runAsQuery } = require("/test/lib/common.sjs");
 const test = require("/test/test-helper.xqy");
+import { TEST_USER_WRITER } from "lib/suiteCommon.mjs";
 
 const targetURI = "/recipes/W20.json";
 
 runAsUpdate(() => addCommentToRecipe(targetURI, "ab1234", "comment 1", xs.dateTime("2023-01-01T00:00:00Z")));
 
-let actual = fn.head(runAsQuery(() => getCommentsFromRecipe(targetURI)));
+let actual = fn.head(runAsQuery(() => getCommentsFromRecipe(targetURI), { "userId": xdmp.user(TEST_USER_WRITER) }));
 
 let assertions = [
   test.assertTrue(Array.isArray(actual), "response should have been an Array"),
