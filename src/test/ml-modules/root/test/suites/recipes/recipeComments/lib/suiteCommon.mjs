0db794ffe7c80ed016f135678db2961234994235
David Cassel <dmcassel@gmail.com>
2023-03-27 15:46:43 -0400

    Specify test usernames

---
+++ src/test/ml-modules/root/test/suites/recipes/recipeComments/lib/suiteCommon.mjs
@@ -0,0 +1,2 @@
+export const TEST_USER_READER = "%%mlAppName%%-test-user-reader-d77f8d15-1ee7-42f2-9f5d-7a48266a558b";
+export const TEST_USER_WRITER = "%%mlAppName%%-test-user-writer-d77f8d15-1ee7-42f2-9f5d-7a48266a558b";
