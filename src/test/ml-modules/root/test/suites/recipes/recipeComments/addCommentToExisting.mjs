f8b87bf452db894db9862badc80f869e3ca2ab0b
David Cassel <dmcassel@gmail.com>
2023-03-29 15:02:32 -0400

    remove user from test

--- src/test/ml-modules/root/test/suites/recipes/recipeComments/addCommentToExisting.mjs
+++ src/test/ml-modules/root/test/suites/recipes/recipeComments/addCommentToExisting.mjs
@@ -3,13 +3,12 @@ declareUpdate();
 const { addCommentToRecipe, getCommentsFromRecipe } = require("/lib/recipes.sjs");
 const { runAsUpdate, runAsQuery } = require("/test/lib/common.sjs");
 const test = require("/test/test-helper.xqy");
-import { TEST_USER_WRITER } from "lib/suiteCommon.mjs";
 
 const targetURI = "/recipes/W20.json";
 
 runAsUpdate(() => addCommentToRecipe(targetURI, "ab1234", "comment 1", xs.dateTime("2023-01-01T00:00:00Z")));
 
-let actual = fn.head(runAsQuery(() => getCommentsFromRecipe(targetURI), { "userId": xdmp.user(TEST_USER_WRITER) }));
+let actual = fn.head(runAsQuery(() => getCommentsFromRecipe(targetURI)));
 
 let assertions = [
   test.assertTrue(Array.isArray(actual), "response should have been an Array"),
